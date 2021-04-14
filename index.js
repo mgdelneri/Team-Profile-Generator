d9c9627bbb44da506a76a0b655edcd149522ebbb
Marina <mgdelneri@gmail.com>
2021-04-14 18:26:25 -0500

    Added generateHTML file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const generateHTML = require('./src/generateHTML');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
