e3b3bef552e7ecd921613086e1e7447335429f36
Marina <mgdelneri@gmail.com>
2021-04-14 17:50:19 -0500

    Added require where necessary, connecting modules

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
